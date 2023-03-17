60f95b01896933836679413f79103746bdd47102
bczsalba <bczsalba@gmail.com>
2023-03-17 14:13:52 +0100

    [palette]: Add `palette` module
    This is mostly just a zenith palette, but it has its keys renamed to
    sound more UI-y.

---
+++ celadon/palette.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+from zenith import Color, Palette
+
+
+palette = Palette(Color.from_hex("#AFE1AF").darken(2))
+
+palette.color_mapping = {
+    "text": Color((245, 245, 245)).darken(2),
+    "primary": palette.primary,
+    "secondary": palette.secondary,
+    "accent": palette.quaternary,
+    "panel1": palette.surface1.darken(3),
+    "panel2": palette.surface2.darken(3),
+    "panel3": palette.surface4.darken(3),
+}
+
+palette.alias()
