124cca6e3b86d87acc88df5ea620c489db95550c
bczsalba <bczsalba@gmail.com>
2023-09-24 19:58:06 +0200

    [widgets]: Add new `Link` widget
    Thanks @JordanShurmer for the suggestion!

--- celadon/widgets/__init__.py
+++ celadon/widgets/__init__.py
@@ -2,4 +2,5 @@ from .button import Button
 from .checkbox import Checkbox
 from .container import *
 from .text import Text
+from .link import Link
 from .widget import *
---
+++ celadon/widgets/link.py
@@ -0,0 +1,18 @@
+from typing import Any
+
+from .text import Text
+
+
+class Link(Text):
+    """A purpose-made Text object for hyperlinks."""
+
+    rules = """
+    Link:
+        height: 1
+    """
+
+    def __init__(self, content: str, to: str | None = None, **widget_args: Any) -> None:
+        if to is None:
+            to = content.lower()
+
+        super().__init__(f"[~/{to}]{content}[/~]", **widget_args)
